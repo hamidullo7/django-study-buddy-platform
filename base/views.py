f5aec6eccc76c94f092247004f1e34b551b7f495
hamidullo <hamidullo@localhost.localdomain>
2022-07-22 15:36:39 +0000

    begining of 'mobile responsiveness'

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from zmq import Message
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # rooms = [
 #     {"id": 1, "name": "Let's learn python!"},
@@ -174,6 +174,13 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    
-    context = {}
-    return render(request, 'base/update-user.html')
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = { 'form': form}
+    return render(request, 'base/update-user.html', context)
